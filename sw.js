aa1ec31f8e2cc3d39d6102ad2f01d462ac145310
jinwenjunzhuo <jinwenjunzhuo@users.noreply.github.com>
2026-05-18 14:08:03 +0300

    地狱锻炉 HELL FORGE - 健身训练计划 PWA

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE = 'hellforge-v1';
+const URLS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(URLS)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)));
+    })
+  );
+});
